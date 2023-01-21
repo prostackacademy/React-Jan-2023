8c63d3b02fec2957fcf0e60719e365e369fbfe7b
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-01-21 12:43:55 +0530

    okie

---
+++ backup-3/PropsStateEx/src/components/Message-1.js
@@ -0,0 +1,8 @@
+function Message() {
+    return <section>
+        <h3>Messaage Component</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quis illum exercitationem, asperiores accusantium consequuntur enim, rerum omnis sequi quae qui nemo odit doloribus dignissimos, beatae delectus amet. Doloribus, iste.</p>
+    </section>
+}
+
+export default Message
